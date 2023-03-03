8a2431c55932b232e83abc6e47aa0df94693306d
wsz <24433.com>
2023-03-03 14:45:45 +0800

    second commit

--- src/main/java/com/wsz/FirstDemo.java
+++ src/main/java/com/wsz/FirstDemo.java
@@ -3,5 +3,6 @@ package com.wsz;
 public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("git~git~git~");
+        System.out.println("git2~git2~git2~");
     }
 }
