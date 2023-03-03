f0da5f5e7dd4b2c79b86bf21b1dd632d262988ff
wsz <24433.com>
2023-03-03 15:12:53 +0800

    hot-fix commit first

--- src/main/java/com/wsz/FirstDemo.java
+++ src/main/java/com/wsz/FirstDemo.java
@@ -3,5 +3,8 @@ package com.wsz;
 public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("git~git~git~");
+        System.out.println("git3~git~git~");
+        System.out.println("git4~git~git~");
+        System.out.println("git5~git~git~");
     }
 }
